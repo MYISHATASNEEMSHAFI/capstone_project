a0cb0cdbc6619183c9a7993f58bb4b7c38d585e8
myisha <myisha@gmail.com>
2026-06-27 08:30:06 +0530

    fixing prod-degrading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,7 +55,7 @@ function App() {
 
             <h1 className="text-center mb-4">
 
-                Notes Management System
+                Myisha's Notes Management System
 
             </h1>
 
